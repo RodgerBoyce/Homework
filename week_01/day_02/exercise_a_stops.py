496327af7c2486309931f12eacccc08904c2c81d
Rodger Boyce <rodgerdboyce@gmail.com>
2023-02-28 16:50:08 +0000

    homework

--- week_01/day_02/exercise_a_stops.py
+++ week_01/day_02/exercise_a_stops.py
@@ -1,6 +1,6 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
-#1. Add "Edinburgh Waverley" to the end of the list
+#1. Add "Edinburgh Waverley" to the end of the listttt
 #2. Add "Glasgow Queen St" to the start of the list
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 #4. Print out the index position of "Linlithgow"
